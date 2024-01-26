4ee01bab30ccab95049ab344f7d3f1fe1a03a1c6
PC <PC@DESKTOP-RU9U602>
2024-01-26 16:30:23 +0530

    Signed-off-by: PC <PC@DESKTOP-RU9U602>

--- src/test/java/runner/testrunner.java
+++ src/test/java/runner/testrunner.java
@@ -5,7 +5,7 @@ import io.cucumber.testng.CucumberOptions;
     
 @CucumberOptions(tags = "@tag1", features = {"src/test/java/features/Cucumber.feature"}, glue = {"seleniumgluecode"},
                  plugin = {"pretty",
-                         "json:target/cucumberjson/cucumberjson.json"},publish=false)
+                         "json:Results/cucumberjson/cucumber.json"},publish=false)
     
 public class testrunner extends AbstractTestNGCucumberTests {
     
